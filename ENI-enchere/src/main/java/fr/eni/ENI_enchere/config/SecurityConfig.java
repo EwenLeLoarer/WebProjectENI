e7c40a3c3e1cfd0ded0f41e3e5acbfd75176e8da
eleloarer2024 <eleloarer2024@129SE8-451MMS3.ad.campus-eni.fr>
2025-02-24 13:43:26 +0100

    test Add template

---
+++ ENI-enchere/src/main/java/fr/eni/ENI_enchere/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package fr.eni.ENI_enchere.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+	
+
+
+}
