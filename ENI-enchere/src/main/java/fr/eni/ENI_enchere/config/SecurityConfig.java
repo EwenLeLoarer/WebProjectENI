aecee981d4df5f2935ada20689dc0c7874744400
eleloarer2024 <eleloarer2024@129SE8-451MMS3.ad.campus-eni.fr>
2025-02-24 14:58:01 +0100

    init du spring security

--- ENI-enchere/src/main/java/fr/eni/ENI_enchere/config/SecurityConfig.java
+++ ENI-enchere/src/main/java/fr/eni/ENI_enchere/config/SecurityConfig.java
@@ -4,11 +4,28 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+	@Bean
+	UserDetailsManager userDetailsManager(DataSource dataSource) {
+		JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+		
+		manager.setUsersByUsernameQuery("select pseudo, password, "
+								+ "active from utilisateur where pseudo = ? ");
+		
+		manager.setAuthoritiesByUsernameQuery(("select pseudo, [role] from [role] "
+											+ "where pseudo=?"));
+		
+		return manager;
+	}
 	
 
 
