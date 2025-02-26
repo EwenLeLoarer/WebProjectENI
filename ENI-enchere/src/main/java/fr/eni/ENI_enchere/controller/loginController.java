7ccea30d49a06564b10dfa92b7f346dd384ed921
Ewen <ewenlelf@gmail.com>
2025-02-26 16:32:01 +0100

    login template

--- ENI-enchere/src/main/java/fr/eni/ENI_enchere/controller/loginController.java
+++ ENI-enchere/src/main/java/fr/eni/ENI_enchere/controller/loginController.java
@@ -1,12 +1,17 @@
 package fr.eni.ENI_enchere.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class loginController {
 	@GetMapping("/login")
-	public String login() {
+	public String login(Model model, HttpServletRequest request) {
+		model.addAttribute("request", request);
 		return "login";
 	}
 }
