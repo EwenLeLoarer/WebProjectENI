74b20c3ba3e1f99059ce363e21bde13a7ae89ab4
Ewen LE LOARER <ewen.leloarer2024@campus-eni.fr>
2025-02-28 15:43:22 +0100

    view other profils

--- ENI-enchere/src/main/java/fr/eni/ENI_enchere/controller/GlobalControllerAdvice.java
+++ ENI-enchere/src/main/java/fr/eni/ENI_enchere/controller/GlobalControllerAdvice.java
@@ -1,13 +1,22 @@
 package fr.eni.ENI_enchere.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import fr.eni.ENI_enchere.bo.Utilisateur;
+import fr.eni.ENI_enchere.service.UtilisateurService;
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+	@Autowired
+	UtilisateurService userService;
+	
 	@ModelAttribute("currentPage")
 	public String getCurrentPage(HttpServletRequest request) {
 		String uri = request.getRequestURI();
